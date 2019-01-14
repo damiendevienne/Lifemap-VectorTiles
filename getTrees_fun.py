5c02ae999ac828ed7a3f14654935600d8e2b15c7
Damien de Vienne <damien.de-vienne@univ-lyon1.fr>
2019-01-14 16:58:12 +0100

    made it an independent function and switched to python3

--- getTrees_fun.py
+++ getTrees_fun.py
@@ -1,9 +1,38 @@
 #!/usr/bin/python
 
-	# from ete3 import Tree
-	# import os
-	# import cPickle as pickle
+import os
+from argparse import ArgumentParser, FileType ##for options handling
+from ete3 import Tree
 
+
+parser = ArgumentParser(description='This function retrieves the lastest NCBI taxonomy for all or a subset of subtrees, and adds french translations. It returns trees in Extended Newick Format (NHX)')
+parser.add_argument('--updatedb', nargs='?', const='True', default='True', help='Should the NCBI taxonomy db be updated? Default is True', choices=['True','False'])
+parser.add_argument('--simplify', nargs='?', const='True', default='False', help='Should the tree be simplified by removing environmental and unindentified species? Default is False', choices=['True','False'])
+parser.add_argument('--taxid', nargs='+', default='1', help='List of taxids for which to return a tree, space-separated. If none is provided, the whole NCBI taxonomy tree is returned and the tree file is called "All.tre". If one or more is provided, the ouput files are named "*taxid*.tre". As a reminder, taxid for Bacteria=2, Eucaryotes=2759 and Archaea=2157.')
+
+args = parser.parse_args()
+
+def updateDB():
+	print('Updating databases...')
+	os.system("wget ftp://ftp.ncbi.nlm.nih.gov/pub/taxonomy/taxdump.tar.gz -N")
+	os.system("tar xvzf taxdump.tar.gz -C taxo/")
+	#unzip taxref NOTE: taxref has to be downloaded by hand beforehand ?
+	os.system("unzip -o taxo/TAXREF_INPN_v11.zip -d taxo/")
+
+def simplify(arbre):
+	initialSize = len(arbre)
+	for n in arbre.traverse():
+		if (n.is_leaf()==True) and (n.rank=='no rank'):
+			n.detach()
+		else:
+			if ('Unclassified' in n.sci_name) or ('unclassified' in n.sci_name) or ('uncultured' in n.sci_name) or ('Uncultured' in n.sci_name) or ('unidentified' in n.sci_name) or ('Unidentified' in n.sci_name) or ('environmental' in n.sci_name) or ('sp.' in n.sci_name):
+				n.detach()
+	print("Tree HAS BEEN simplified")
+	finalSize = len(arbre)
+	diffInSize = (initialSize-finalSize)
+	print(str(diffInSize) + " tips have been removed (" + str(round(float(diffInSize)/float(initialSize)*100, 2)) + '%)')
+	print("FINAL TREE SIZE: " + str(finalSize))
+	return arbre
 
 def getTheTrees(): 
 	##DOWNLOAD taxdump and store in taxo folder
@@ -14,21 +43,21 @@ def getTheTrees():
 			self.common_name_FR = []
 
 
-	print "Getting french translations..."
+	print("Getting french translations...")
 	TRANS = {} ##translations in french
 	with open("taxo/TAXREFv11.txt") as f:  
 		for line in f:
 			sciname = line.split("\t")[14]
 			comnameFR = line.split("\t")[19]
-			if (TRANS.has_key(sciname)==False and line.split("\t")[19]!=''):
+			if (sciname not in TRANS and line.split("\t")[19]!=''):
 				TRANS[sciname] = Trans()
 			if (line.split("\t")[19]!=''):
 				TRANS[sciname].common_name_FR.append(comnameFR)
 
 	#get translation of ranks
-	print "\nGetting rank names in french..."
+	print("Getting rank names in french...")
 	RANKS = {}
-	with open("ranks.txt") as f:  
+	with open("taxo/ranks_FR.txt") as f:  
 		for line in f:
 			rank_en = line.split("\t")[0]
 			rank_fr = line.split("\t")[1].rstrip() ##to remove \n
@@ -48,18 +77,18 @@ def getTheTrees():
 	cpt = 0
 	cptfr = 0
 	ATTR = {} ##here we will list attribute of each species per taxid
-	print "Reading NCBI taxonomy..."
+	print("Reading NCBI taxonomy...")
 	with open("taxo/names.dmp") as f:  
 		for line in f:		
 			taxid = line.split("|")[0].replace("\t","")
 			tid_val = line.split("|")[1].replace("\t","")
 			tid_type = line.split("|")[3].replace("\t","")
-			if (ATTR.has_key(taxid)==False):
+			if (taxid not in ATTR):
 				ATTR[taxid] = Taxid()
 			if (tid_type=="scientific name"):
 				ATTR[taxid].sci_name = tid_val
 				#and get translation in french (if any)
-				if TRANS.has_key(tid_val):
+				if tid_val in TRANS:
 					ATTR[taxid].common_name_FR = TRANS[tid_val].common_name_FR
 					cptfr += 1
 			if (tid_type=="authority"):
@@ -84,16 +113,15 @@ def getTheTrees():
 	T = {}
 
 	###New gettrees
-	from ete3 import Tree
 	filepath = 'taxo/nodes.dmp'  
-	print "Building the NCBI taxonomy tree..."
+	print("Building the NCBI taxonomy tree...")
 	with open(filepath) as fp:  
 		first_line = fp.readline() ## remove the 1 | 1 edge
 		for line in fp:
 			dad = line.split("|")[1].replace("\t","")
 			son = line.split("|")[0].replace("\t","")
 			rank = line.split("|")[2].replace("\t","")
-			if (T.has_key(dad)==False):
+			if (dad not in T):
 				T[dad] = Tree()
 				T[dad].name = dad
 #				T[dad].rank = rank
@@ -104,7 +132,7 @@ def getTheTrees():
 				T[dad].synonym = ATTR[dad].synonym
 				T[dad].authority = ATTR[dad].authority
 				T[dad].common_name_FR = ATTR[dad].common_name_FR
-			if (T.has_key(son)==False):
+			if (son not in T):
 				T[son] = Tree()
 				T[son].name = son
 				T[son].rank = rank
@@ -122,45 +150,21 @@ def getTheTrees():
 			T[dad].add_child(T[son])
 	return T
 
-# ##we save T entirely so that we do not hacve to write it to a file.
-# print("\n>>> Writing ARCHAEA tree...")
-# with open('ARCHAEA.pkl', 'wb') as output:
-#     pickle.dump(T['2157'], output, pickle.HIGHEST_PROTOCOL)
-# print("\n>>> Writing BACTERIA tree...")
-# with open('BACTERIA.pkl', 'wb') as output:
-#     pickle.dump(T['2'], output, pickle.HIGHEST_PROTOCOL)
-# print("\n>>> Writing EUKA tree...")
-# with open('EUKARYOTES.pkl', 'wb') as output:
-#     pickle.dump(T['2759'], output, pickle.HIGHEST_PROTOCOL)
-# print(">>> DONE")
 
+if (args.updatedb=='True'):
+	updateDB()
 
-# #t = T['1']
-# tarc = T['2157']
-# tbac = T['2']
-# teuc = T['2759']
+T = getTheTrees() #Get the Whole tree, for real.
 
-# print("\n>>> Writing ARCHAEA tree...")
-# tarc.write(outfile = "ARCHAEA", features = ["name", "taxid", "sci_name","common_name","rank", "authority","synonym","common_name_FR", "rank_FR"], format_root_node=True)
-# print("\n>>> Writing BACTERIA tree...")
-# tbac.write(outfile = "BACTERIA", features = ["name", "taxid", "sci_name","common_name","rank", "authority","synonym","common_name_FR", "rank_FR"], format_root_node=True)
-# print("\n>>> Writing EUKA tree...")
-# teuc.write(outfile = "EUKARYOTES", features = ["name", "taxid", "sci_name","common_name","rank", "authority","synonym","common_name_FR", "rank_FR"], format_root_node=True)
-# print(">>> DONE")
+if (len(args.taxid)==1):
+	print("Writing the whole NCBI taxonomy tree to All.tre...")
+	tout=T[args.taxid]
+	tout.write(outfile = "All.tre", features = ["name", "taxid", "sci_name","common_name","rank", "authority","synonym","common_name_FR", "rank_FR"], format_root_node=True)
+else:
+	print("Writing trees...")
+	for i in args.taxid:
+		out= i + ".tre"	
+		print("  " + out)
+		t=T[i]
+		t.write(outfile = out, features = ["name", "taxid", "sci_name","common_name","rank", "authority","synonym","common_name_FR", "rank_FR"], format_root_node=True)
 
-
-
-
-# RANKS = {}
-
-# for n in t.traverse(): 
-# 	if RANKS.has_key(n.rank)== False:
-# 		RANKS[n.rank] = 1
-# 	else: 
-# 		RANKS[n.rank] = RANKS[n.rank] + 1
-
-# ranks = open("ranks.txt", "w")
-# for k in RANKS:
-# 	ranks.write("%s\t%s\n" % (k,RANKS[k]))
-
-# ranks.close()
